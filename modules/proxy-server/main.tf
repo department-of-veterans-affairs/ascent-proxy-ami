120515bee53574b83b6b11731ae234f8a25a3df3
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-07-18 16:03:31 -0500

    PLAT-257: correct typo

--- modules/proxy-server/main.tf
+++ modules/proxy-server/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "proxy_instance" {
   associate_public_ip_address = "${var.associate_public_ip_address}"
   vpc_security_group_ids = ["${aws_security_group.proxy_security_group.id}", "${var.aws_security_group_ids}"]
   user_data              = "${var.user_data == "" ? data.template_file.proxy_user_data.rendered : var.user_data}"
-  tags = "${merge(var.tags, map("Name", "${var.instance-name}"))}"
+  tags = "${merge(var.tags, map("Name", "${var.instance_name}"))}"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
