bb851075d4a7713e2ac3b3d28ae519404a2b2db8
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-08-01 09:04:38 -0500

    trim the double quotes from the vault token and do a debug output

--- modules/proxy-server/proxy-user-data.sh
+++ modules/proxy-server/proxy-user-data.sh
@@ -9,7 +9,10 @@ sed -i 's|SERVER_NAME|${server_name}|g' /tmp/templates/ca.crt.tpl
 sed -i 's|SERVER_NAME|${server_name}|g' /tmp/templates/server.crt.tpl
 sed -i 's|SERVER_NAME|${server_name}|g' /tmp/templates/server.key.tpl
 
-VAULT_TOKEN=$(curl --insecure -X POST "${vault_address}/v1/auth/aws/login" -d '{"role":"vetservices","pkcs7":"'$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d '\n')'","nonce":"5defbf9e-a8f9-3063-bdfc-54b7a42a1f95"}' | jq '.auth.client_token')
+VAULT_TOKEN=$(curl --insecure -X POST "${vault_address}/v1/auth/aws/login" -d '{"role":"vetservices","pkcs7":"'$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d '\n')'","nonce":"5defbf9e-a8f9-3063-bdfc-54b7a42a1f95"}' | jq '.auth.client_token' | tr -d '"')
+
+echo "<<DEBUG>>"
+echo "[consul-template -once -config="/tmp/templates/consul-template-config.hcl" -vault-addr=${vault_address} -vault-token=$${VAULT_TOKEN}]"
 
 consul-template -once -config="/tmp/templates/consul-template-config.hcl" -vault-addr=${vault_address} -vault-token=$${VAULT_TOKEN}
 
